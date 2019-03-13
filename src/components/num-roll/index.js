df817691d9faff2f77ec240a79e3b185bb944f52
xiaqingsong <xqslove@yeah.net>
2019-03-13 12:00:07 +0800

    新增num-roll组件

---
+++ src/components/num-roll/index.js
@@ -0,0 +1,67 @@
+Component({
+    properties: {
+        value: {
+            type: Number,
+            value: 0
+        },
+        grid: {
+            type: Number,
+            value: 6
+        }
+    },
+    data: {
+        numArr: [],
+        number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+    ready() {
+        this.initNum();
+
+        setTimeout(() => {
+            this.doAnimation();
+        }, 500)
+    },
+    methods: {
+        display(info) {
+            this.setData({
+                ...info
+            })
+        },
+        /*
+        * 初始化数据
+        * */
+        initNum() {
+            const numStr = this.prefixInteger(this.data.value, this.data.grid);
+            this.data.numArr = numStr.split('').map(v => ({num: +v}));
+            this.setData({
+                numArr: this.data.numArr
+            });
+        },
+        /*
+        * 滚动动画
+        * */
+        doAnimation() {
+            const animation = wx.createAnimation({
+                duration: 1800,
+                timingFunction: 'ease',
+            });
+
+            const {numArr} = this.data;
+            numArr.forEach(v => {
+                if (v) {
+                    v.animationData = animation.translateY(`-${v.num}0%`).step().export();
+                }
+            });
+
+            this.setData({
+                numArr
+            });
+        },
+        /*
+        * 数字前补0
+        * */
+        prefixInteger(num, length) {
+            return (Array(length).join('0') + num).slice(-length);
+        }
+
+    }
+});
